e4660fe242f71c35230b699b2e7e868c3a453c0e
Sirat-chan <sirat.benjemaa@edu.isetcom.tn>
2023-04-11 22:59:24 +0100

    testing authentification with Jest

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,7 +1,8 @@
 import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
-import { mongoose } from'mongoose';
+import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
@@ -11,7 +12,14 @@ import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
+app.set('trust proxy', true);
 app.use(json());
+app.use(
+    cookieSession({
+        signed: false,
+        secure: true
+    })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -25,16 +33,19 @@ app.all('*', async (req, res) => {
 app.use(errorHandler);
 
 const start = async () => {
+    if (!process.env.JWT_KEY) {
+        throw new Error('JWT_KEY must be defined');
+    }
     try {
-        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
-        });
-        console.log('Connected to MongoDB');
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {});
+        console.log('Connected to MongoDb');
     } catch (err) {
         console.error(err);
     }
+
     app.listen(3000, () => {
         console.log('Listening on port 3000!!!!!!!!');
     });
 };
 
-
+start();
